e827be97246f43401c294436fcd0039adb41b57f
NguyenVanThanh <ryuran22002600@gmail.com>
2024-06-26 00:04:28 +0700

    video 31 - Cach su dung bien so voi JSX

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,10 +1,19 @@
 import './style.css';
 
 const MyComponent = () => {
+    const stringType = "pseudo text"
+    const numberType = 123
+    // const booleanType = true
+    // const nullType = null
+    // const undefinedType = undefined
+    const objectType = {
+        name: "Nguyen van A",
+        age: 20
+    }
     return (
         <>
-            <div>Super cấp vip pro</div>
-            <div className='child'>Word is red</div>
+            <div> {stringType} {numberType} {objectType.name} {JSON.stringify(objectType)}&Super cấp vip pro</div>
+            <div className='child' style={{ fontSize: '40px' }}>Word is red</div>
         </>
     )
 }
