7d5e86526a5935a8fb53ff9eec50bfecf669300f
NguyenVanThanh <ryuran22002600@gmail.com>
2024-07-09 05:45:15 +0700

    v33 reset app file

---
+++ src/components/TodoData.jsx
@@ -0,0 +1,13 @@
+
+const TodoData = () => {
+    return (
+        <div>
+            <div className='todo-data'>
+                <div> Learning React</div>
+                <div> Watching Youtube</div>
+            </div>
+        </div>
+    )
+}
+
+export default TodoData
